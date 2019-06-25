4ad0366bd82c99bd33fc62c89f914be0a76d65a9
matt <matt@ontargetinteractive.com>
2019-06-24 21:33:19 -0500

    use exec instead of ssh2

--- actions.php
+++ actions.php
@@ -1,13 +1,10 @@
 <?php
-if (!function_exists('ssh2_connect')) {
-    echo "<b>php-ssh2</b> is not installed, this page will only work with installed php-ssh2.<br />\n";
-    exit();
-}
+
 
 $active_page = 'actions';
 
 include_once('./include/config.php');
-	
+
 switch ($_REQUEST['action']) {
 	case 'kill_pid':
 		if($_REQUEST['pid']>0)
@@ -53,16 +50,7 @@ switch ($_REQUEST['action']) {
 			$message = 'Service Name shouldn\'t be an empty value';
 		}
 	break;
-	/*case 'update_sources':
-		$cmd = 'sudo apt-get update';
-		$message = 'Command "'.$cmd.'" executed';
 
-	break;
-	case 'update_firmware':
-		$cmd = 'sudo rpi-update';
-		$message = 'Command "'.$cmd.'" executed';
-		
-	break;*/
 	case 'reboot':
 		$cmd = 'sudo reboot';
 		$message = 'Command "'.$cmd.'" executed';
@@ -77,12 +65,12 @@ switch ($_REQUEST['action']) {
 		{
 			$message = 'Command shouldn\'t be an empty value';
 		}
-		
+
 	break;
 }
 ?>
 <?php
-	
+
 	if(!file_exists(dirname(__FILE__).'/command_logs'))
 	{
 		$cmd = "sudo mkdir -m 777 ".dirname(__FILE__)."/command_logs && sudo chmod -R 777 ".dirname(__FILE__)."/command_logs";
@@ -92,18 +80,19 @@ switch ($_REQUEST['action']) {
 	{
 		$cmd = "sudo chmod -R 777 ".dirname(__FILE__)."/command_logs";
 	}
-	
-	
+
+
 	if(!empty($cmd))
 	{
-		$connection = ssh2_connect('localhost', SSH_PORT);
-		ssh2_auth_password($connection, SSH_USER, SSH_PASS);
-		$stream = ssh2_exec($connection, $cmd);
-		stream_set_blocking($stream, true);
-		$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
-		$message .= '<br/><b>Command output:</b><br/>'.nl2br(stream_get_contents($stream_out));
-		
-		ssh2_exec($connection, 'exit');
+//		$connection = ssh2_connect('localhost', SSH_PORT);
+//		ssh2_auth_password($connection, SSH_USER, SSH_PASS);
+//		$stream = ssh2_exec($connection, $cmd);
+//		stream_set_blocking($stream, true);
+//		$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+//		$message .= '<br/><b>Command output:</b><br/>'.nl2br(stream_get_contents($stream_out));
+//
+//		ssh2_exec($connection, 'exit');
+	    $message = system($cmd);
 	}
 ?>
 <!DOCTYPE html>
@@ -120,11 +109,11 @@ switch ($_REQUEST['action']) {
 	<link href="./static/css.php" rel="stylesheet" type="text/css">
 	<script src="./static/js.php" type="text/javascript"></script>
 	<script>
-		
+
 $( document ).ready(function() {
-	
+
 	$("#advanced_command").submit(function( event ) {
-		
+
 		if(confirm("Are you sure you want to execute this command?"))
 		{
 			if($("#background_command").is(':checked'))
@@ -135,10 +124,10 @@ $( document ).ready(function() {
 					alert("command sent!");
 					$('#cmd').val('');
 				});
-				
-				
-				
-				
+
+
+
+
 				return false;
 			}
 			else
@@ -151,26 +140,26 @@ $( document ).ready(function() {
 			event.preventDefault();
 			return false;
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 	});
-	
-	
-	
-	
-	
+
+
+
+
+
 });
 
 
 function delete_log(file)
 {
-		
+
 		if(confirm("Are you sure you want to delete this log?"))
 		{
-			
+
 				event.preventDefault();
 				$.post( "ajax.php", {'action':'delete_log','log_file':file} )
 				.done(function( data ) {
@@ -180,17 +169,17 @@ function delete_log(file)
 		}
 		else
 		{
-			
+
 		}
 
 }
-		
+
 	</script>
 </head>
 
 <body>
 <div class="container">
-	
+
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -212,7 +201,7 @@ function delete_log(file)
 		</div><!--/.container-fluid -->
 	</nav>
 
-	
+
 
 				<div id="system-status" class="panel panel-default" style="margin-bottom: 5px">
 					<div class="panel-heading">
@@ -224,9 +213,9 @@ function delete_log(file)
 							if(!empty($message))
 							{
 								echo '<div class="alert alert-info" role="alert" style="margin-bottom:20px;">'.$message.'</div>';
-							}	
+							}
 						?>
-						
+
 						<form method="post">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label" for="pid">Kill Process by ID</label>
@@ -239,10 +228,10 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Kill</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
+
 						<form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label" for="pname">Kill Processes by Name</label>
@@ -255,11 +244,11 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Kill</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-								
-								
+
+
 						<form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label" for="sname">Start Service by Name</label>
@@ -272,11 +261,11 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Start</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
-						
+
+
 						<form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label" for="sname">Stop Service by Name</label>
@@ -289,10 +278,10 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Stop</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
+
 						<!--form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label">Update Sources</label>
@@ -301,10 +290,10 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Update</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
+
 						<form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label">Update Firmware</label>
@@ -313,10 +302,10 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">Update</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form-->
-						
+
 						<form method="post" style="margin-top: 20px;">
 							<div class="form-group row">
 								<label class="col-sm-2 form-control-label">Reboot RPi System</label>
@@ -325,10 +314,10 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure to reboot RPi?')">Reboot</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
+
 						<hr/>
 						<form method="post" id="advanced_command" style="margin-top: 20px;">
 							<div class="form-group row">
@@ -347,12 +336,12 @@ function delete_log(file)
 									<button type="submit" class="btn btn-primary">Execute</button>
 								</div>
 							</div>
-							
-							
+
+
 						</form>
-						
+
 						<hr/>
-						
+
 						<div class="well well-sm" id="command_logs"><h3>Useful commands:</h3>
 							<code>sudo python /folder_path_to_file/mypython_file.py</code> to execute a python script.<br/>
 							<code>sudo apt-get update</code> to update RPi sources.<br/>
@@ -362,34 +351,34 @@ function delete_log(file)
 							<code>sudo chmod 0777 <?php echo dirname(__FILE__); ?>/include/config.php</code> to make the config file writable in case you can't write any data to it.
 
 						</div>
-						
+
 						<?php
-							
+
 							$directory = dirname(__FILE__).'/command_logs/';
 							$scanned_directory = array_diff(scandir($directory), array('..', '.'));
 
 							if(is_array($scanned_directory))
 							{
-							
+
 								echo '<div class="well well-sm" id="command_logs"><h3>Background command output logs:</h3>';
 								foreach($scanned_directory AS $file)
 								{
 									echo '<div class="'.str_replace(".", "_", $file).'"><a href="./command_logs/'.$file.'">'.$file.'</a> '. date ("F d Y H:i:s.", filemtime('./command_logs/'.$file)).' <a href="javascript:void(0);" onclick="javascript:delete_log(\''.$file.'\');" title="delete_log" style="color:red;"><i class="fa fa-trash-o" aria-hidden="true"></i></a></div>';
 								}
-								
+
 								echo '</div>';
-						
+
 							}
-						
+
 						?>
-						
+
 					</div>
-				
-				
+
+
 				</div>
-				
-				
-		
+
+
+
 </div>
 
 <footer class="footer">
